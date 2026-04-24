b3873b1ccf637045931282e60d959d65b871e69a
iHamza14 <anwarhamza1408@gmail.com>
2026-04-24 23:35:40 +0530

    deploy1

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,23 +2,25 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/route";
 import "./scheduler/cron";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
-app.use(cookieParser())
-app.use(express.json());
 
+app.use(cookieParser());
+app.use(express.json());
 
 // mount routes
 app.use("/api", router);
 
-const PORT =3000 ;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 export default app;
